af1a16a5ce25dbb31e98c63900fbcd60a83c6d57
Ariana Cabana <acabana.0904@gmail.com>
2017-03-24 16:25:11 -0500

    Validando Input

--- js/main.js
+++ js/main.js
@@ -3,58 +3,89 @@ function validateForm(){
   var nombre = document.getElementById("name");
   var apellido = document.getElementById("lastname");
   var correo = document.getElementById("input-email");
-  var contraseña = document.getElementById("input-password");
+  var psw = document.getElementById("input-password");
   var opcion = document.getElementById("bici");
-  var select = opcion.options[opcion.selectedIndex].value;
 
-  //var caracteresvalidos = /^[a-z][\w.-]+@\w[\w.-]+.[\w.-]*[a-z][a-z]$/i;
+  var crtCantidad = /^\s+|\s+$/;
+  var crtNumero = /[0-9]/;
 
-  //valido el nombre
-  function validaNombre(){
-  if (nombre.value.length < 2){
-    alert("Tiene que escribir su nombre");
-    return nombre;
+  validaNombre();
+
+//valido el nombre
+function validaNombre(){
+  if (nombre.value.length < 2 || crtCantidad.test(nombre.value)){
+    alert("Tiene que ingresar su nombre");
+    return nombre.focus();
+  } else if (crtNumero.test(nombre.value)){
+    alert("Solo se permiten letras");
+    return nombre.focus();
+  } else if (!(/[^a-z]+([a-z]{1,})/.test(nombre.value))){
+    alert("Su nombre debe empezar con mayúscula");
+    return nombre.focus();
+  } else{
+    validaApellido();
   }
-} validaNombre();
+}
 
-  //valido el apellido
-  function validaApellido(){
-  if (apellido.value.length < 2){
-    alert("Tiene que escribir su apellido");;
-    return 0;
+//valido el apellido
+function validaApellido(){
+  if (apellido.value.length < 2 || crtCantidad.test(apellido.value)){
+    alert("Tiene que ingresar su apellido");
+    return apellido.focus();
+  } else if (crtNumero.test(apellido.value)){
+    alert("Solo se permite letras");
+    return apellido.focus();
+  } else if (!(/[^a-z]+([a-z]{1,})/.test(apellido.value))){
+    alert("Su apellido debe empezar con mayúscula");
+    return apellido.focus();
+  } else{
+    validaEmail();
+
   }
-} validaApellido();
+}
 
-  //valido el correo
-  function validaEmail(){
-    var crt = /\S+@\+\S+.\+S+/;
-  if (!crt.test(correo) && correo.value.length< 10){
-      alert("Ingrese el formato correcto del correo");
-      return 0;
-    }
-} validaEmail();
+//valido el correo
+function validaEmail(){
+  var crt = /\S+@\+\S+.\+S+/;
+  var crt2= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+(\w{2,4})+$/;
 
-  //valido la contraseña
-  function validaPass(){
-    if (contraseña.value.length<6){
-      alert("Ingrese una contraseña de minimo 6 caracteres");
-      return 0;
-    }
+  if (!crt.test(correo.value) && correo.value.length < 10){
+    alert("Ingrese el formato correcto del correo");
+    return correo.focus();
+  } else if (!(crt2.test(correo.value))){
+    alert ("Correo no válido");
+    return correo.focus();
+  }
+  else{
+    validaPass();
+  }
+}
 
-  } validaPass()
+//valido la contraseña
+function validaPass(){
+  if (psw.value.length < 6 || crtCantidad.test(psw.value)){
+    alert("Ingrese una contraseña de mínimo 6 carácteres");
+    return psw.focus();
+  } else if(psw.value == "123456"){
+    alert("La contraseña debe ser diferente de 123456");
+    return psw.focus();
+  } else if(psw.value == "098754"){
+    alert("La contraseña debe ser diferente de 098754");
+    return psw.focus();
+  } else{
+    validaOpciones();
+  }
+}
 
-  //valido el interés
-  function validaOpciones(){
-    //var op = sel.options[sel.selectedIndex].text;
+//valido el interés
+function validaOpciones(){
+  var op = opcion.options[opcion.selectedIndex].text;
     if(opcion.value == 0){
-      alert("Eliga una opción");}
-    else {
-      alert ("Usted a elegido: "+ select);
+      alert("Eliga una opción");
+      return opcion.focus();
     }
-  } validaOpciones()
-
-
-      /*if(opcion.value == null || opcion.value == "") {
-      alert("Porfavor, seleccione su color favorito");
-    }*/
+    else {
+      alert ("Usted a elegido: "+ op + " \n Gracias por su visita :)");
+    }
+  }
 }
